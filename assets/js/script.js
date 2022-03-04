f701ede656c49318b14c56e0e931f03c1a724d5d
J-R77-stack <johnnyramsay81@gmail.com>
2022-03-04 12:53:36 +0000

    alter html css and images for game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,18 @@
 
 
 /* select the ninja, sumo and score from the document by getting element by Id*/
-const ninja = document.getElementById("ninja");
-const sumo = document.getElementById("sumo");
-const score = document.getElementById("score");
+
 
 /*Function to define what happens when the ninja jumps adding jump-animation to fuction from css*/
 
-function jump() {
-  ninja.classList.add("jump-ninja");
-  setTimeout(() => {
-    ninja.classList.remove("jump-ninja");
-  }, 500);
-}
+
 
 /*Event listener to use the jump function on pressing a key and to stop user pressing jump 
 until original jump is finished*/
 
-document.addEventListener("keypress", () => {
-  if (!ninja.classList.contains("jump-ninja")) {
-    jump();
-  }
-});
+
 
 /* set interval game loop to remove sumo wrester at end of game screen and determine collision
 recognition between sumo wrester and ninja*/
 
-setInterval(() => {
-  score.innerText++;
-  const ninjaTop = parseInt(
-    window.getComputedStyle(ninja).getPropertyValue("top")
-  );
-  const sumoLeft = parseInt(
-    window.getComputedStyle(sumo).getPropertyValue("left")
-  );
 
-  if (sumoLeft < 0) {
-    sumo.style.display = "none";
-  } else {
-    sumo.style.display = "";
-  }
-
-  if (sumoLeft < 35 && sumoLeft > 0 && ninjaTop > 100) {
-    alert("GAME OVER");
-    location.reload();
-  }
-}, 50);
